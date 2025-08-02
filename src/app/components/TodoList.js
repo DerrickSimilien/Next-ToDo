406ba4c674adcfbd9244451b0d39c9815696b20e
Derrick <similienderrick@yahoo.com>
2025-08-02 09:41:53 -0400

    Initialized git repository

---
+++ src/app/components/TodoList.js
@@ -0,0 +1,15 @@
+export default function TodoList () {
+   return (
+      <>
+      <div>
+        <h1>Todos</h1>
+        <ul>
+            <li>Todo 1</li>
+            <li>Todo 2</li>
+            <li>Todo 3</li>
+            <li>Todo 4</li>
+        </ul>
+      </div>
+      </>
+   )
+}
